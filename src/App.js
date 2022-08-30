ef2926073b84563a7fc60c71762f6690db826106
AZIZUL HAQUE TOUSIF <meta.block.tousif@gmail.com>
2022-08-30 11:29:34 +0600

    Dependency Instalation completed

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  return (
+    <div className="h-screen bg-dark-blue bg-main bg-contain">
+      <h1 className="text-5xl text-center p-4 font-mono font-black text-white">
+        React Initial Project set-up
+      </h1>
+      <h4 className="text-2xl p-2 text-center font-mono font-black text-white">
+        Tech: Tailwind, React-router, react-toastify,
+        daisyUI,react-spinner-loader, React Hook Form.
+      </h4>
+      <ToastContainer />;
+    </div>
+  );
+}
+export default App;
