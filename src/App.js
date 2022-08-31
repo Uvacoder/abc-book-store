0a9d402a05f8fa21fa07aec08bc2a8d886bff4dd
AlAminKh03 <alaminkhan6203@gmail.com>
2022-08-31 09:14:21 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -4,15 +4,7 @@ import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
     <div className="h-screen bg-dark-blue bg-main bg-contain">
-      <h1 className="text-5xl text-center p-4 font-mono font-black text-white">
-        React Initial Project set-up
-      </h1>
-      <h4 className="text-2xl p-2 text-center font-mono font-black text-white">
-        Tech: Tailwind, React-router, react-toastify,
-        daisyUI,react-spinner-loader, React Hook Form.
-      </h4>
-      <h6>Please Check any dependency need to install</h6>
-      <ToastContainer />;
+      <h1>I have just made my repo</h1>
     </div>
   );
 }
