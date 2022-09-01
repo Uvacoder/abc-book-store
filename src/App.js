30f9c46b7939a1e3d91bd8a71b82717f996dc4c6
AlAminKh03 <alaminkhan6203@gmail.com>
2022-09-01 20:43:21 +0600

    reviews added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "react-toastify/dist/ReactToastify.css";
+import Books from "./Pages/Books";
 
 function App() {
   return (
     <div className="h-screen bg-dark-blue bg-main bg-contain">
       <h1>I have just made my repo</h1>
+      <Books></Books>
     </div>
   );
 }
---
+++ src/Pages/Books.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './Books.module.css'
+
+const Books = () => {
+    return (
+        <div className={`${styles.container} w-64 bg-base-100 shadow-xl m-4`}>
+            <div>
+                <figure><img className={styles.bookCover} src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                <div>
+                    <button className={styles.click}>Click Me!</button>
+                </div>
+            </div>
+            <div className='bookInfo p-2'>
+                <h2>A Mystery</h2>
+                <p>by<span className='text-lime-600'> Lawrence Bendict</span></p>
+            </div>
+        </div>
+    );
+};
+
+export default Books;
