181afcbc6a8fa9d1e0cf310ef016478588d15a0f
Jobayer_ak <jobayer.ak@gmail.com>
2022-09-08 22:42:56 +0600

    new line added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <h1> This is new line</h1>
+      <h1> This is new line</h1>
+      <h1> This is new line</h1>
+      <h1> This is new line</h1>
+    </div>
+  );
 };
 
 export default Home;
