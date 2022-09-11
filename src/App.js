0396628ba47fc344226e99287448be626f5e319e
AZIZUL HAQUE TOUSIF <meta.block.tousif@gmail.com>
2022-09-12 01:53:41 +0600

    Header initialized

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Login from "./Pages/Register/Login";
-import SignUp from "./Pages/Register/SignUp";
+import Header from "./SharedCompo/Header";
 
 function App() {
   return (
     <div>
-      <Login></Login>
-      <SignUp></SignUp>
-      {/* bg-dark-blue bg-main bg-contain */}
-      {/* <h1 className="text-5xl text-center p-4 font-mono font-black text-white">
-        React Initial Project set-up
-      </h1>
-      <h4 className="text-2xl p-2 text-center font-mono font-black text-white">
-        Tech: Tailwind, React-router, react-toastify,
-        daisyUI,react-spinner-loader, React Hook Form.
-      </h4>
-      <h6>Please Check any dependency need to install</h6> */}
+      <Header/>
       <ToastContainer />
     </div>
   );
