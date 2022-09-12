a2b8b671357dac0f04e9a2e444243f58743fa009
anishazahan <anishazahan13@gmail.com>
2022-09-12 19:45:43 +0600

    book details content

--- src/App.js
+++ src/App.js
@@ -3,22 +3,17 @@ import "react-toastify/dist/ReactToastify.css";
 import BookDetails from "./components/Books/BookDetails.jsx";
 import Login from "./components/SignIn/Login.jsx";
 import SignUp from "./components/SignUp/SignUp";
+import Review from "./SharedCompo/Review/Review.jsx";
 
 function App() {
   return (
     <div className="h-screen">
       <BookDetails></BookDetails>
+      <Review></Review>
+
     {/* <Login></Login>
       <SignUp></SignUp> */}
-      {/* bg-dark-blue bg-main bg-contain */}
-      {/* <h1 className="text-5xl text-center p-4 font-mono font-black text-white">
-        React Initial Project set-up
-      </h1>
-      <h4 className="text-2xl p-2 text-center font-mono font-black text-white">
-        Tech: Tailwind, React-router, react-toastify,
-        daisyUI,react-spinner-loader, React Hook Form.
-      </h4>
-      <h6>Please Check any dependency need to install</h6> */}
+    
       <ToastContainer />
     </div>
   );
--- src/SharedCompo/Review/Review.jsx
+++ src/SharedCompo/Review/Review.jsx
@@ -1,12 +1,53 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react'
+import './Review.css'
 
 const Review = () => {
+    const [reviews,setReviews]= useState([]);
+     useEffect(()=>{
+        fetch('review.json')
+        .then(res=>res.json())
+        .then(data=>setReviews(data))
+
+     },[])
   return (
-    <div>
+
+    <div className="">
+        <h3 className='text-2xl font-semibold text-center my-8'>Our client review</h3>
+    <div className='grid grid-cols-3 gap-3 max-w-7xl mx-auto my-16'>
+       
+
+        {
+            reviews.map(singleReview => {
+                const {img,name,review}=singleReview;
+                // return <div className="border p-3">
+                //    <div className="flex items-center space-x-3">
+                //    <div className="w- h-[100px] rounded-full">
+                //         <img src={img} alt="" />
+                //     </div>
+                //     <div className="review">
+                //         <p className='text-gray-400 text-sm'>{review}</p>
+                //     </div>
+                //    </div>
+                //    <p>By- <span className='text-black text-semibold'>{name}</span> </p>
+                // </div>
+              return  <div class="review-card card card-side bg-base-100 rounded-0 ">
+                <figure><img className='w-[150px] h-full' src={img} alt=""/></figure>
+                <div class="card-body w-full">
+                    <h2 class="text-pink-800">5 star</h2>
+                    <p className='text-sm text-gray-500'>{review}</p>
+                    <div class="card-actions flex justify-end">
+                   <p className='text-right text-sm'>By- <span className='text-xs font-bold '>{name}</span> </p>
+                    </div>
+                </div>
+                </div>
+            })
+        }
 
 
 
-        
+    </div>
     </div>
   )
 }
