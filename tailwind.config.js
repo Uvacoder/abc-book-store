d63eb2d0ff973ef4e79ffc71109920836842f039
AZIZUL HAQUE TOUSIF <meta.block.tousif@gmail.com>
2022-09-14 18:58:36 +0600

    --

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,6 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        "dark-blue": "#0b1120",
-      },
-      backgroundImage: {
-        main: "url('/src/assets/images/bg.png')",
-      },
-    },
   },
   plugins: [require("daisyui")],
 };
