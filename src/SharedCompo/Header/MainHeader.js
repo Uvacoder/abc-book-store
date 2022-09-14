3ab90c23bfa8cd4900aba0a8cb4b2d5d86ef8195
AlAminKh03 <alaminkhan6203@gmail.com>
2022-09-15 00:36:26 +0600

    navbar completed

--- src/SharedCompo/Header/MainHeader.js
+++ src/SharedCompo/Header/MainHeader.js
@@ -2,14 +2,14 @@ import React from "react";
 import SignUp from "./SignUp";
 import SearchBar from "./SearchBar";
 import DropDown from "./Dropdown";
-// import TopOffer from "./TopOffer";
+import TopOffer from "./TopOffer";
 
 const MainHeader = () => {
   return (
     <>
       <div className="bg-[#3e5962]">
         <div className="w-3/5 my-auto mx-auto">
-          {/* <TopOffer /> */}
+          <TopOffer />
         </div>
       </div>
       <SignUp />
