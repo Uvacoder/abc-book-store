1215e7cfc1dd28be6c6264acafcecbb89664a0dc
AlAminKh03 <alaminkhan6203@gmail.com>
2022-09-16 00:25:05 +0600

    nicely done editors

--- src/Pages/Editorial/Editorial.js
+++ src/Pages/Editorial/Editorial.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
+import './Editorial.modules.css'
 
 const Editorial = () => {
     const [readMoreShown, setReadMoreShown] = useState(false);
@@ -12,62 +13,59 @@ const Editorial = () => {
                 <div className='text-center'>
                     <h1 className='text-2xl font-[ubuntu] py-3'>Editorial Reviews</h1>
                 </div>
-                <div className='text-sm text-center'>
+                <div className='text-sm text-center px-52'>
                     <div>
                         <p>
-                            <span>
-                                A New York Times Bestseller <br />
-                                An Entertainment Weekly Best YA Book of the Year Selection<br />
-                                A Buzzfeed Best YA Book of the Year Selection<br />
-                                A New York Public Library's Best Book for Teens Selection<br />
-                                A Popcrush Best Young Adult Book of the Year Selection<br />
-                                A CBC Teen Choice Book Award Nominee<br />
-                                A Bustle Best Young Adult Book of the Month<br />
-                                A Goodreads Best Young Adult Book of the Year Nominee<br />
-                                A YALSA Top Ten Best Fiction Book Nominee<br />
-                                A YALSA Quick Pick for Reluctant Young Adult Readers<br />
-                            </span>
-
-                            <span>“Pretty Little Liars meets The Breakfast Club....so make room for One of Us Is Lying in your bags, because this is one carry-on you won’t want to put down." —Entertainment Weekly Online</span>
-
-                            <span>“You’ll tear through this juicy, super-fun (if murder can ever be fun?) thriller."—Bustle</span>
-
-                            <span>
-                                "A whodunit with a Breakfast Club twist...following four unique voices on a chase to find the killer, this one will keep you guessing until the very, very end."—Popcrushs
-                            </span>
+                            A New York Times Bestseller <br />
+                            An Entertainment Weekly Best YA Book of the Year Selection<br />
+                            A Buzzfeed Best YA Book of the Year Selection<br />
+                            A New York Public Library's Best Book for Teens Selection<br />
+                            A Popcrush Best Young Adult Book of the Year Selection<br />
+                            A CBC Teen Choice Book Award Nominee<br />
+                            A Bustle Best Young Adult Book of the Month<br />
+                            A Goodreads Best Young Adult Book of the Year Nominee<br />
+                            A YALSA Top Ten Best Fiction Book Nominee<br />
+                            A YALSA Quick Pick for Reluctant Young Adult Readers<br />
                         </p>
-                    </div>
-                    <div className={`${readMoreShown ? "block" : "hidden"}`}>
+
+                        <p>“Pretty Little Liars meets The Breakfast Club....so make room for One of Us Is Lying in your bags, because this is one carry-on you won't want to put down." —   <i>Entertainment Weekly Online</i> </p><br />
+
+                        <p>“You'll tear through this juicy, super-fun (if murder can ever be fun?) thriller."— <i>Bustle</i> </p><br />
+
                         <p>
-                            <span>
-                                "Twisty plotting, breakneck pacing and intriguing characterisation add up to an exciting, single-sitting thrillerish treat."—The Guardian
-                            </span>
+                            "A whodunit with a Breakfast Club twist...following four unique voices on a chase to find the killer, this one will keep you guessing until the very, very end."— <i>Popcrushs</i>
+                        </p><br />
+                    </div>
+                    <div className={`${readMoreShown ? "block" : "hidden"} transition ease-in `}>
+                        <p>
+                            "Twisty plotting, breakneck pacing and intriguing characterisation add up to an exciting, single-sitting thrillerish treat."— <i>The Guardian</i>
+                        </p><br />
 
-                            <span>
-                                "This is no ordinary whodunit…surprising and relevant."—USA Today</span>
+                        <p>
+                            "This is no ordinary whodunit…surprising and relevant."— <i>USA Today</i> </p><br />
 
-                            <span> "Readers will have a hard time putting this clever page-turner down." —BookPage Online</span>
+                        <p> "Readers will have a hard time putting this clever page-turner down." — <i>BookPage Online</i> </p><br />
 
-                            <span>  “An addictive, devour-in-one-sitting thriller with so many twists and turns you'll be wondering until the very end: Who really killed Simon?”—Kara Thomas, author of The Darkest Corners and Little Monsters</span>
+                        <p>  “An addictive, devour-in-one-sitting thriller with so many twists and turns you'll be wondering until the very end: Who really killed Simon?”— <i>Kara Thomas</i>, author of The Darkest Corners and Little Monsters</p><br />
 
-                            <span>
-                                ★"[As] McManus's intense mystery unfolds...each character becomes more complex and nuanced, adding richness and depth to the suspense." —VOYA, Starred Review
-                            </span>
+                        <p>
+                            ★"[As] McManus's intense mystery unfolds...each character becomes more complex and nuanced, adding richness and depth to the suspense." — <i>VOYA, Starred Review</i>
+                        </p><br />
 
-                            <span>"This fast-paced blend of Gossip Girl, Pretty Little Liars, and classic John Hughes will leave readers racing to the finish as the try to unravel the mystery on their own."—Kirkus Reviews</span>
+                        <p>"This fast-paced blend of Gossip Girl, Pretty Little Liars, and classic John Hughes will leave readers racing to the finish as the try to unravel the mystery on their own."— <i>Kirkus Reviews</i></p>
 
-                            <span>
-                                "One of Us Is Lying is flat-out addictive...[McManus] weaves an authentic, suspenseful mystery that readers can imagine taking place at their very own high school.”—RT Book Reviews
-                            </span>
+                        <p><br />
+                            "One of Us Is Lying is flat-out addictive...[McManus] weaves an authentic, suspenseful mystery that readers can imagine taking place at their very own high school.”— <i>RT Book Reviews</i>
+                        </p><br />
 
-                            <span>
-                                "A smart, twisted, and unpredictable YA mystery that will have readers guessing until the very end."—SLJ
-                            </span>
+                        <p>
+                            "A smart, twisted, and unpredictable YA mystery that will have readers guessing until the very end."— <i>SLJ</i>
+                        </p><br />
 
-                            <span>"An engaging, enticing look at the pressures of high school and the things that cause a person to lose control."—Booklist
-                            </span>
-                            From the Publisher
-                        </p>
+                        <p>"An engaging, enticing look at the pressures of high school and the things that cause a person to lose control." <i>—Booklist</i>
+                        </p><br />
+                        From the Publisher
+
                     </div>
 
                 </div>
