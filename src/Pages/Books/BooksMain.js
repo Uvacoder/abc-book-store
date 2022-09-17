d53940bcdf6ff82250906b05c40fdc25f623199b
AZIZUL HAQUE TOUSIF <meta.block.tousif@gmail.com>
2022-09-17 14:25:27 +0600

    Books routre's API connected and data shown

--- src/Pages/Books/BooksMain.js
+++ src/Pages/Books/BooksMain.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import ProductCarousel from './ProductCarousel';
 
 const BooksMain = () => {
     return (
-        <div>
-            <h1 className='text-3xl text-center m-4'>This is Books routes</h1>
-        </div>
+      <div className="pt-2 pb-2">
+        <h1 className="m-2 text-center text-2xl text-gray-900">
+          Top Rated General Books
+        </h1>
+        <ProductCarousel/>
+        <h1 className="m-2 text-center text-2xl text-gray-900">
+          New Arrivals Books
+        </h1>
+        <ProductCarousel />
+        <h1 className="m-2 text-center text-2xl text-gray-900">
+          All General Books
+        </h1>
+        <ProductCarousel />
+      </div>
     );
 };
 
