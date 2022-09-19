2b75795bfb68cdc31c375f1ea50101f36b6caf71
mohainakonda <mohasinakonda39@gmail.com>
2022-09-19 11:50:28 +0600

    toys feature added

--- src/Pages/ToysGames/ToysGamesMain.js
+++ src/Pages/ToysGames/ToysGamesMain.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
+import ToysCarousel from "../ToysAndGame/ToysCarousel";
 
 const ToysGamesMain = () => {
-    return (
-      <div>
-        <h1 className="text-3xl text-center m-4">This is Toys &#38; Games Books routes</h1>
-      </div>
-    );
+  return (
+    <div>
+      <h1 className="text-3xl text-center m-4">
+        This is Toys &#38; Games Books routes
+      </h1>
+      <ToysCarousel />
+    </div>
+  );
 };
 
 export default ToysGamesMain;
